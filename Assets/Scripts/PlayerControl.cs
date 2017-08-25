258c50d82cad50dbf3ff4cf0d1cc10fbda2102ed
Lego Chiang <qas612820704@gmail.com>
2017-08-25 22:19:13 +0800

    Basic Player Control

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+	
+	public float speed;
+
+	private Rigidbody2D player;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		player.AddForce (new Vector2 (h, v) * speed);
+	}
+}
